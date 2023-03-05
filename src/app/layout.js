051ef8b2b7f8bda79c8f4d252f9782a8c5bf303e
Nicholas Carter <github@nicholascarter.dev>
2023-03-05 11:24:13 +0000

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-import './globals.css'
+import "./globals.css";
+import { AnalyticsWrapper } from "@/components/analytics.component";
 
 export const metadata = {
-  title: 'Formula 1 2023 calendar',
-  description: 'Upcoming Formula One race calendars and driver standings',
-}
+  title: "Formula 1 2023 calendar",
+  description: "Upcoming Formula One race calendars and driver standings",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <AnalyticsWrapper />
+      </body>
     </html>
-  )
+  );
 }
